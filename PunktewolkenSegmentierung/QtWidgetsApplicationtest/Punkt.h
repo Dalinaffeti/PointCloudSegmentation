adf57a9ecdbf4314eb8faa68451754bbb72568cd
s0570269 <s0570269@htw-berlin.de>
2021-11-25 17:08:20 +0100

    Klassen added

---
+++ PunktewolkenSegmentierung/QtWidgetsApplicationtest/Punkt.h
@@ -0,0 +1,15 @@
+#pragma once
+class Punkt
+{
+	public:
+		float X;
+		float Y;
+		float Z;
+		Punkt(float x, float y, float z) {
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+};
+
---
+++ PunktewolkenSegmentierung/QtWidgetsApplicationtest/Punktwolke.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <iterator>
+#include "Punkt.h"
+
+using namespace std;
+class Punktwolke
+{
+public:
+	list<Punkt> punkte;
+};
+
