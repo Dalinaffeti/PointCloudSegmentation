c4a43f0e5a14eeb662ec853902565337d22d9dd2
s0570269 <s0570269@htw-berlin.de>
2021-11-27 12:49:36 +0100

    statusBar+ProgressBar

--- PunktewolkenSegmentierung/QtWidgetsApplicationtest/PunktwolkenSegmentierung.cpp
+++ PunktewolkenSegmentierung/QtWidgetsApplicationtest/PunktwolkenSegmentierung.cpp
@@ -1,7 +1,15 @@
 #include "PunktwolkenSegmentierung.h"
 #include "pclviewer.h"
+#include <QProgressBar>
 PunktwolkenSegmentierung::PunktwolkenSegmentierung(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    
+    ui.statusBar->showMessage("whazzup");
+    QProgressBar* progressBar = new QProgressBar(this);
+    progressBar->setMinimum(0);
+    progressBar->setMaximum(100);
+    progressBar->setValue(80);
+    ui.statusBar->addPermanentWidget(progressBar);
 }
